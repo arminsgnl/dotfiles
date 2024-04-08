2a25857ce1d48fec7c65448be195be75c05e0c96
arminsgnl <113938110+arminsgnl@users.noreply.github.com>
2024-04-08 12:13:01 -0400

    disabled Mac stuff

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
-ln -sf "${DOTFILES_LOCATION}/vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
+# ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+# ln -sf "${DOTFILES_LOCATION}/vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
 
 CODE_EXTENSIONS=(
   42crunch.vscode-openapi
   # bmewburn.vscode-intelephense-client
-  CircleCI.circleci
+  # CircleCI.circleci
   DavidAnson.vscode-markdownlint
   dbaeumer.vscode-eslint
   esbenp.prettier-vscode
